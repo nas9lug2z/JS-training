de2ac55049912a148fb8b6ce65f395e371be297c
Nastia <64069789+nas9lug2z@users.noreply.github.com>
2020-09-08 21:00:46 +0200

    Delete 7.todolist.js

--- 7.todolist.js
+++
@@ -1,8 +0,0 @@
-const todoEl = document.querySelector("ul");
-const todosEl = document.getElementsByClassName("todos");
-
-// console.log(todoEl.firstElementChild);
-// console.log(todosEl[0].parentElement);
-
-
-console.log(todoEl.firstElementChild.nextElementSibling);
